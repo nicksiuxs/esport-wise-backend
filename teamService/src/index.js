7350507203eee99ed78a0f7b8cd64207b1152f38
Nicolas Londono <nicksiuxs@gmail.com>
2024-11-18 15:55:10 -0500

    Add: cors microservice

--- teamService/src/index.js
+++ teamService/src/index.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const cors = require('cors');
 const teamController = require("./controllers/teamController");
 
 const app = express();
+app.use(cors());
+
 app.use(morgan("dev"));
 app.use(express.json());
 
